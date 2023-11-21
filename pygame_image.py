aa98c72a7ce78085b04b37dfed8380d5d2db1775
c0b22019 <c0b220193a@edu.teu.ac.jp>
2023-11-21 15:56:38 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -4,8 +4,9 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((1600, 900))
+    screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
@@ -16,12 +17,15 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = -(tmr % 1600)
+        
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img, [1600+x, 0])
         screen.blit(kk_imgs[tmr%2], [300, 200])
 
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
